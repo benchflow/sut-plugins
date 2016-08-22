72ecaca20fde279be0c679840948672c4e4a1d2b
simonedavico <simonedavico@gmail.com>
2016-08-22 21:56:49 +0200

    Add java.util.logging import to wfms plugin

--- wfms/camunda/7.4.0-7.5.0/WfMSPlugin.java
+++ wfms/camunda/7.4.0-7.5.0/WfMSPlugin.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import java.util.TreeMap;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.logging.*;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
